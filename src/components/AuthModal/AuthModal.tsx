4af51935a2d02eda95f287e047a68026eb424b2c
SasunHakobyan <sasunhakobyan2002@gmail.com>
2023-02-07 17:32:28 +0400

    auth form validation

--- src/components/AuthModal/AuthModal.tsx
+++ src/components/AuthModal/AuthModal.tsx
@@ -4,49 +4,60 @@ import {authSlice} from "../../store/reducers/authReducer";
 import {IUser} from "../../models/user";
 import {modalSlice} from "../../store/reducers/modalReducer";
 import {useDispatch} from "react-redux";
+import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
+import styles from './AuthModal.module.css';
 
 interface IAuthModalProps {
     toggleModal: (showModal: boolean) => void;
 }
 
 const AuthModal = (props: IAuthModalProps) => {
-    const [usernameValue, setUsernameValue] = useState('');
-    const [passwordValue, setPasswordValue] = useState('');
-
     const dispatch = useDispatch();
 
-    const formSubmitHandler: FormEventHandler<HTMLFormElement> = (e) => {
-        e.preventDefault();
+    const {
+        register,
+        formState: {
+            errors,
+        },
+        handleSubmit
+    } = useForm();
 
+    const formSubmitHandler: SubmitHandler<FieldValues> = (data) => {
         const user: IUser = {
             id: 1,
-            username: usernameValue
+            username: data.username
         }
 
         dispatch(authSlice.actions.login(user));
         dispatch(modalSlice.actions.setShowModal(false));
     }
 
-    const usernameValueChangeHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
-        const username = e.currentTarget.value;
-        setUsernameValue(username);
-    }
-
-    const passwordValueChangeHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
-        const password = e.currentTarget.value;
-        setPasswordValue(password);
-    }
-
     return (
         <Modal toggleModal={props.toggleModal}>
-            <form onSubmit={formSubmitHandler}>
-                <div>
-                    <input onChange={usernameValueChangeHandler} type='text' placeholder='username' />
+            <form onSubmit={handleSubmit(formSubmitHandler)}>
+                <div className={styles.formControl}>
+                    <label>Username</label>
+                    <input {...register('username', {
+                        required: 'Username is required',
+                        minLength: {
+                            value: 4,
+                            message: 'Username length must be minimum 4'
+                        }
+                    })}/>
+                    {errors?.username && <span className={styles.errorMsg}>{errors?.username?.message?.toString()}</span>}
                 </div>
-                <div>
-                    <input onChange={passwordValueChangeHandler} type='text' placeholder='password'/>
+                <div className={styles.formControl}>
+                    <label>Password</label>
+                    <input {...register('password', {
+                        required: 'Password is required',
+                        minLength: {
+                            value: 4,
+                            message: 'Password length must be minimum 4'
+                        }
+                    })}/>
+                    {errors?.password && <span className={styles.errorMsg}>{errors?.password?.message?.toString()}</span>}
                 </div>
-                <button type='submit'>Login</button>
+                <button className={styles.submitBtn} type='submit'>Login</button>
             </form>
         </Modal>
     );
