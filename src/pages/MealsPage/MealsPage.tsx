3a37cfc441863a842ee92569011b1b1a1155c6bd
SasunHakobyan <sasunhakobyan2002@gmail.com>
2023-02-08 11:17:38 +0400

    Meals Page

--- src/pages/MealsPage/MealsPage.tsx
+++ src/pages/MealsPage/MealsPage.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from './MealsPage.module.css';
+import MealGrid from "../../components/MealGrid/MealGrid";
+import MainContent from "../../layout/MainContent/MainContent";
 
 const MealsPage = () => {
     return (
-        <div>
-
-        </div>
+        <MainContent>
+            <div>
+                <h3>KFC</h3>
+                <MealGrid/>
+            </div>
+            <div>
+                <h3>Walgreens</h3>
+                <MealGrid/>
+            </div>
+            <div>
+                <h3>Taco Bell</h3>
+                <MealGrid/>
+            </div>
+        </MainContent>
     );
 };
 
