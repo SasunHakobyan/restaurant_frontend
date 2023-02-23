0b5b7316c62677c71237785ec42bd20c95e430c6
SasunHakobyan <sasunhakobyan2002@gmail.com>
2023-02-23 18:45:16 +0400

    modal outside close on process

--- src/layout/Modal/Modal.tsx
+++ src/layout/Modal/Modal.tsx
@@ -8,13 +8,16 @@ interface IModalProps {
 }
 
 const Modal = (props: IModalProps) => {
+    const outsideClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+        console.log(e.currentTarget)
+    }
+
     return (
-        <div className={styles.modal}>
+        <div onClick={outsideClickHandler} className={styles.modal}>
             <div style={props.customStyles && props.customStyles} className={styles.modalContent}>
                 <button onClick={props.closeModal} className={styles.closeBtn}>&times;</button>
                 {props.children}
             </div>
-
         </div>
     );
 };
